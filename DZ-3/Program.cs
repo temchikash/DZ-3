1ad9f65b27fac019ea6cde386279ff5fc543d392
Гарифуллин Амир <amirgarifullin@gmail.com>
2023-09-28 20:23:15 +0300

    Добавьте файлы проекта.

---
+++ DZ-3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная\n");
+
+            try
+            {
+                int year_days, day;
+                int year = 2023;
+
+                Console.Write("Введите количество дней в году: ");
+                year_days = int.Parse(Console.ReadLine());
+                Console.Write($"Введите номер дня (от 1 до {year_days}): ");
+                day = int.Parse(Console.ReadLine());
+
+                if ((year_days != 366) && (year_days != 365))
+                {
+                    throw new Exception($"В году не может быть {year_days} дней!");
+                }
+
+                if ((day < 1) || (day > year_days))
+                {
+                    throw new Exception($"Вы ввели число меньше 1 или больше {year_days}");
+                }
+
+                if (year_days % 5 != 0)
+                {
+                    year += 1;
+                }
+
+                DateTime dateTime = new DateTime(year, 1, 1).AddDays(day - 1);
+
+                switch (dateTime.Month)
+                {
+                    case 01:
+                        Console.WriteLine($"{dateTime.Day} Января");
+                        break;
+                    case 02:
+                        Console.WriteLine($"{dateTime.Day} Февраля");
+                        break;
+                    case 03:
+                        Console.WriteLine($"{dateTime.Day} Марта");
+                        break;
+                    case 04:
+                        Console.WriteLine($"{dateTime.Day} Апреля");
+                        break;
+                    case 05:
+                        Console.WriteLine($"{dateTime.Day} Мая");
+                        break;
+                    case 06:
+                        Console.WriteLine($"{dateTime.Day} Июня");
+                        break;
+                    case 07:
+                        Console.WriteLine($"{dateTime.Day} Июля");
+                        break;
+                    case 08:
+                        Console.WriteLine($"{dateTime.Day} Августа");
+                        break;
+                    case 09:
+                        Console.WriteLine($"{dateTime.Day} Сентября");
+                        break;
+                    case 10:
+                        Console.WriteLine($"{dateTime.Day} Октября");
+                        break;
+                    case 11:
+                        Console.WriteLine($"{dateTime.Day} Ноября");
+                        break;
+                    case 12:
+                        Console.WriteLine($"{dateTime.Day} Декабря");
+                        break;
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Вы ввели не целое число");
+            }
+            catch (Exception dateException)
+            {
+                Console.WriteLine(dateException.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Homework
+{
+    enum Cards
+    {
+        Шестерка = 6,
+        Семерка,
+        Восьмерка,
+        Девятка,
+        Десятка,
+        Валет,
+        Дама,
+        Король,
+        Туз
+    }
+    enum Days
+    {
+        Понедельник = 1,
+        Вторник,
+        Среда,
+        Четверг,
+        Пятница,
+        Суббота,
+        Воскресенье
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1 не смог\nЗадание 2\nВведите номер карты от 6 до 14\n");
+            try
+            {
+                if (!byte.TryParse(Console.ReadLine(), out byte inputNumber) || (inputNumber < 6 || inputNumber > 14))
+                {
+                    throw new ArgumentException("Номер карты должен быть от 6 до 14\n");
+                }
+                else
+                {
+                    Console.WriteLine((Cards)inputNumber);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Задание 3\nКто вы?");
+
+            switch (Console.ReadLine().ToLower())
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila\n");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol\n");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer\n");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine\n");
+                    break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars\n");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal\n");
+                    break;
+                default:
+                    Console.WriteLine("Beer\n");
+                    break;
+            }
+
+            Console.WriteLine("Задание 4.\nВведите номер дня недели от 1 до 7.");
+            try
+            {
+                if (int.TryParse(Console.ReadLine(), out int Day) || Day < 1 || Day > 7)
+                {
+                    throw new ArgumentException("Неправильный номер\n");
+                }
+                else
+                {
+                    Console.WriteLine($"День недели соответствующий номеру {Day} это {(Days)Day}\n");
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Задание 5");
+            string[] dollsNames = { "Hello Kitty", "Doll", "Doll", "Hello Kitty", "Doll", "Barbie doll", "Barbie doll", "Doll" };
+            int c = 0;
+            foreach (string el in dollsNames)
+            {
+                if (el == "Barbie doll" || el == "Hello Kitty")
+                {
+                    c++;
+                }
+            }
+            Console.WriteLine($"В сумке лежит {c} кукол.");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
